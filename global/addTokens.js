0f3cde04727a520e9476de9dfd93440f8c49785c
whoppersYT <91441733+whoppersYT@users.noreply.github.com>
2021-11-30 17:47:46 -0800

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -6,11 +6,11 @@ async function getName(authToken) {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (1000 daily)'));
     const myToken = localStorage.token.split('JWT ')[1];
 
-    if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.')
+    if (tokens > 1000) {
+        alert('You can only add up to 1000 tokens daily.')
     }
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
@@ -21,8 +21,8 @@ async function addCurrencies() {
             "authorization": localStorage.token
         },
         body: JSON.stringify({
-            addedTokens: tokens,
-            addedXp: 300,
+            addedTokens: 1000,
+            addedXp: 1000,
             name: await getName(myToken)
         })
     });
